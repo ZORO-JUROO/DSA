9101e3f458a65e4e4275eaf02ffbec69ffa5788c
Sujal Agrawal <113822996+ZORO-JUROO@users.noreply.github.com>
2025-07-21 11:55:18 +0530

    Time: 21 ms (36.66%) | Memory: 45.6 MB (12.29%) - LeetSync

---
+++ DP/322-coin-change/coin-change.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int coinChange(int[] coins, int a) {
+        int n=coins.length;
+        int dp[][]=new int[n][a+1];
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 1; j <= a; j++) {
+            if (j % coins[0] == 0) {
+                dp[0][j] = j / coins[0];
+            } else {
+                dp[0][j] = -1; 
+            }
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<=a;j++)
+            {
+                int prev=dp[i-1][j];
+                if(prev==-1)
+                {
+                    dp[i][j]=-1;
+                    if(coins[i]<=j && dp[i][j-coins[i]]!=-1)
+                    {
+                        dp[i][j]=1+dp[i][j-coins[i]];
+                    }
+                }
+                else
+                {
+                    dp[i][j]=prev;
+                    if(coins[i]<=j && dp[i][j-coins[i]]!=-1)
+                    {
+                        dp[i][j]=Math.min(dp[i][j],1+dp[i][j-coins[i]]);
+                    }
+                }
+            }
+        }
+        return dp[n-1][a];
+    }
+}
