9b0dcb615a39f4d0f559e05c78f61777dca8a704
Sujal Agrawal <113822996+ZORO-JUROO@users.noreply.github.com>
2025-09-03 14:12:51 +0530

    Time: 286 ms (86.84%) | Memory: 0B (100.00%) - LeetSync

--- DP/602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ DP/602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 with r as (select requester_id as id from RequestAccepted union all select accepter_id from RequestAccepted)
-select id,c as num from (select id,count(*) over ( partition by id) as c from r)temp order by c desc limit 1;
+select id,count(*) as num from r group by id order by num desc limit 1;
