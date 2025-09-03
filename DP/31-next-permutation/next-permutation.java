e1d6540f25a3552595e029f4484a9930db6d815b
Sujal Agrawal <113822996+ZORO-JUROO@users.noreply.github.com>
2025-09-03 22:05:43 +0530

    Time: 1 ms (12.40%) | Memory: 43.1 MB (57.01%) - LeetSync

---
+++ DP/31-next-permutation/next-permutation.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void nextPermutation(int[] arr) {
+        int n=arr.length,i;
+        //right se chalu kariyo aur chota ko compare karte ja left walo ke saath until u get arr[k]<arr[m],k<m aur swap karde isse tereko 0 to k ka shortest no. joh ki bada hai orginal arr se woh mil jayega..now now just sort k to n to get shortest no. in k to n. 
+        int k=-1,a=-1,b=-1;
+        outer: for(i=n-1;i>0;i--)
+        {
+
+            for(int j=i-1;j>=0;j--)
+            if(arr[i]>arr[j] && j>k)
+            {
+                k=j;a=i;b=j;
+                break;
+            }
+        }
+        if(k!=-1){
+                int t=arr[a];
+                arr[a]=arr[b];
+                arr[b]=t;}
+        Arrays.sort(arr,k+1,n);
+    }
+}
