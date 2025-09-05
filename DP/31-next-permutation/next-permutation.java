86049c4ebb3555b52c52dd7ba443229c18f83a65
Sujal Agrawal <113822996+ZORO-JUROO@users.noreply.github.com>
2025-09-05 23:06:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (89.14%) - LeetSync

--- DP/31-next-permutation/next-permutation.java
+++ DP/31-next-permutation/next-permutation.java
@@ -1,22 +1,34 @@
 class Solution {
-    public void nextPermutation(int[] arr) {
-        int n=arr.length,i;
-        //right se chalu kariyo aur chota ko compare karte ja left walo ke saath until u get arr[k]<arr[m],k<m aur swap karde isse tereko 0 to k ka shortest no. joh ki bada hai orginal arr se woh mil jayega..now now just sort k to n to get shortest no. in k to n. MAA KI ANKH BEATS 12 PERCENT HI AYA.
-        int k=-1,a=-1,b=-1;
-        outer: for(i=n-1;i>Math.max(0,k);i--)
+    public void reverse(int i,int arr[])
+    {
+        
+        int n=arr.length-1;
+        while(i<n)
         {
-
-            for(int j=i-1;j>=Math.max(0,k);j--)
-            if(arr[i]>arr[j] && j>k)
+            int t=arr[i];arr[i++]=arr[n];arr[n--]=t;
+        }
+    }
+    public void nextPermutation(int[] arr) {
+        int n=arr.length,ind=-1;
+        //find break point
+        for(int i=n-1;i>0;i--)
+        {
+            if(arr[i]>arr[i-1])
             {
-                k=j;a=i;b=j;
+                ind=i-1;
                 break;
             }
         }
-        if(k!=-1){
-                int t=arr[a];
-                arr[a]=arr[b];
-                arr[b]=t;}
-        Arrays.sort(arr,k+1,n);
+        if(ind!=-1)
+        {
+            //swap break point with smallest number on its right that is greater than it
+            for(int i=n-1;i>ind;i--)
+            {
+                if(arr[i]>arr[ind])
+                {int t=arr[i];arr[i]=arr[ind];arr[ind]=t; break;}
+            }
+        }
+        //reverse the rightpart of th break point;
+        reverse(ind+1,arr);
     }
 }
