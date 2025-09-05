46e51ace7d52081e813f55008812bcdc702949fb
Sujal Agrawal <113822996+ZORO-JUROO@users.noreply.github.com>
2025-09-05 22:37:40 +0530

    Time: 1 ms (12.35%) | Memory: 43.1 MB (75.68%) - LeetSync

--- DP/31-next-permutation/next-permutation.java
+++ DP/31-next-permutation/next-permutation.java
@@ -1,12 +1,12 @@
 class Solution {
     public void nextPermutation(int[] arr) {
         int n=arr.length,i;
-        //right se chalu kariyo aur chota ko compare karte ja left walo ke saath until u get arr[k]<arr[m],k<m aur swap karde isse tereko 0 to k ka shortest no. joh ki bada hai orginal arr se woh mil jayega..now now just sort k to n to get shortest no. in k to n. 
+        //right se chalu kariyo aur chota ko compare karte ja left walo ke saath until u get arr[k]<arr[m],k<m aur swap karde isse tereko 0 to k ka shortest no. joh ki bada hai orginal arr se woh mil jayega..now now just sort k to n to get shortest no. in k to n. MAA KI ANKH BEATS 12 PERCENT HI AYA.
         int k=-1,a=-1,b=-1;
-        outer: for(i=n-1;i>0;i--)
+        outer: for(i=n-1;i>Math.max(0,k);i--)
         {
 
-            for(int j=i-1;j>=0;j--)
+            for(int j=i-1;j>=Math.max(0,k);j--)
             if(arr[i]>arr[j] && j>k)
             {
                 k=j;a=i;b=j;
