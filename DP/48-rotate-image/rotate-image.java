a664728cc3b3158bc2e5d68487da76c021c303bd
Sujal Agrawal <113822996+ZORO-JUROO@users.noreply.github.com>
2025-09-06 20:30:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (41.47%) - LeetSync

--- DP/48-rotate-image/rotate-image.java
+++ DP/48-rotate-image/rotate-image.java
@@ -1,19 +1,24 @@
 class Solution {
+    void reverse(int arr[])
+    {
+        int n=arr.length;
+        for(int i=0;i<n/2;i++)
+        {
+            int t=arr[i];arr[i]=arr[n-i-1];arr[n-i-1]=t;
+        }
+    }
     public void rotate(int[][] arr) {
         int n=arr.length;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n/2;j++)
-            {
-                int t=arr[j][i];arr[j][i]=arr[n-j-1][i];arr[n-j-1][i]=t;
-            }
-        }
         for(int i=0;i<n;i++)
         {
             for(int j=i;j<n;j++)
             {
                 int t=arr[j][i];arr[j][i]=arr[i][j];arr[i][j]=t;
             }
+        }
+        for(int i=0;i<n;i++)
+        {
+            reverse(arr[i]);
         }
     }
 }
