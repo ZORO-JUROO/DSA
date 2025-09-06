68a38af2890094e644b8107e641b76eb2a3ef02a
Sujal Agrawal <113822996+ZORO-JUROO@users.noreply.github.com>
2025-09-06 14:35:37 +0530

    Time: 788 ms (10.33%) | Memory: 0B (100.00%) - LeetSync

--- DP/1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ DP/1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 with a as (select count(*) as c from Product)
-select customer_id from Customer group by customer_id having count( distinct product_key) in (select c from a);
+select customer_id from Customer group by customer_id having count( distinct product_key) = (select c from a);
