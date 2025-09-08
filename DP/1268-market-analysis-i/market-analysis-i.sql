ff68549f507786233ac4b1fdef2e841be6d3e4c7
Sujal Agrawal <113822996+ZORO-JUROO@users.noreply.github.com>
2025-09-08 14:55:51 +0530

    Time: 1189 ms (67.59%) | Memory: 0B (100.00%) - LeetSync

--- DP/1268-market-analysis-i/market-analysis-i.sql
+++ DP/1268-market-analysis-i/market-analysis-i.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-with ord as (select count(*) as c,buyer_id as id from Orders where year(order_date)=2019 group by buyer_id)
+with ord as (select count(buyer_id) as c,buyer_id as id from Orders where year(order_date)=2019 group by buyer_id)
 select user_id as buyer_id, join_date,case when c is null then 0 else c end as orders_in_2019 from users left join ord on users.user_id=ord.id; 
