a1c100325f755f215d423923cdccf31a2192aef1
Sujal Agrawal <113822996+ZORO-JUROO@users.noreply.github.com>
2025-09-09 22:06:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (47.48%) - LeetSync

--- DP/50-powx-n/powx-n.java
+++ DP/50-powx-n/powx-n.java
@@ -1,5 +1,18 @@
 class Solution {
+    double mm(double x,int n)
+    {
+         if(n==0) return 1;
+        if(n%2==0)
+        {double k=mm(x,n/2); return k*k;}
+        else
+        {
+            return x * mm(x,n-1);
+        }
+    }
     public double myPow(double x, int n) {
-      return Math.pow(x,n) ;
+       if(n<0){ if(n==Integer.MIN_VALUE) return 1/(x*mm(x,Integer.MAX_VALUE)); else
+       return 1/mm(x,-n);}
+       else
+       return mm(x,n);
     }
 }
