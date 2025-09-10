8313c4f0ce3287bdb969d7cface3102e4350e667
Sujal Agrawal <113822996+ZORO-JUROO@users.noreply.github.com>
2025-09-10 20:30:14 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (36.31%) - LeetSync

--- DP/62-unique-paths/unique-paths.java
+++ DP/62-unique-paths/unique-paths.java
@@ -12,8 +12,16 @@ class Solution {
     }
     public int uniquePaths(int m, int n) {
         int dp[][]=new int[m][n];
-        for(int i=0;i<m;i++)
-        Arrays.fill(dp[i],-1);
-        return path(m-1,n-1,dp);
+        Arrays.fill(dp[0],1);
+        for(int i=1;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int up=(j-1==-1)?0:dp[i][j-1];
+                int left=(i-1==-1)?0:dp[i-1][j];
+                dp[i][j]=up+left;
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
