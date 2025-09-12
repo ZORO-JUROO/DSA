8e214bbc05ac20ab020bb8cfe63bb76f96591802
Sujal Agrawal <113822996+ZORO-JUROO@users.noreply.github.com>
2025-09-12 19:26:34 +0530

    Time: 17 ms (72.66%) | Memory: 44.1 MB (45.92%) - LeetSync

--- DP/18-4sum/4sum.java
+++ DP/18-4sum/4sum.java
@@ -1,19 +1,24 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] arr, int target) {
-        Set<List<Integer>> a=new HashSet<>();
+        ArrayList<List<Integer>> a=new ArrayList<>();
         int n=arr.length;
         Arrays.sort(arr);
         for(int i=0;i<n;i++)
         {
+            if(i!=0 && arr[i]==arr[i-1])
+            {continue;}
             for(int j=i+1;j<n;j++)
             {
+                if(j!=i+1 && arr[j]==arr[j-1])
+            {continue;}
                 int s=j+1,e=n-1;
                 while(s<e)
                 {
                     long sum=(long)arr[i]+arr[j]+arr[s]+arr[e];
                     if(sum==target)
                     {a.add(Arrays.asList(arr[i],arr[j],arr[s],arr[e]));
-                    s++;e--;
+                    s++;e--; while(s<e && arr[s]==arr[s-1]){s++;}
+                    while(s<e && arr[e]==arr[e+1]){e--;}
                     }
                     else if(sum<target)
                         s++;
@@ -22,6 +27,6 @@ class Solution {
                 }
             }
         }
-        return new ArrayList<List<Integer>>(a);
+        return a;
     }
 }
