f688d3b177ad701131fd601759570fe4cc535ecd
Sujal Agrawal <113822996+ZORO-JUROO@users.noreply.github.com>
2025-09-16 21:48:49 +0530

    Time: 11 ms (5.24%) | Memory: 45.4 MB (5.82%) - LeetSync

---
+++ DP/39-combination-sum/combination-sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    void rec(int arr[],int i,ArrayList<List<Integer>> set, ArrayList<Integer> ad,int sum,int target)
+    {
+        if(sum==target)
+        {set.add(ad);return;}
+        if(sum>target)
+        return;
+        for(;i<arr.length;i++)
+        {
+            ArrayList<Integer> a=new ArrayList<>(ad);
+            int m=target/arr[i],s=sum;
+            for(int j=1;j<=m;j++)
+            {
+                a.add(arr[i]);
+                ArrayList<Integer> v=new ArrayList<>(a);
+                s=s+arr[i];
+                rec(arr,i+1,set,v,s,target);
+            }
+        }
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        ArrayList<List<Integer>> set=new ArrayList<>();
+        Arrays.sort(candidates);
+        ArrayList<Integer> a=new ArrayList<>();
+        rec(candidates,0,set,a,0,target);
+        return set;
+    }
+}
