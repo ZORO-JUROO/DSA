86f058ff8808d82e1cd7d8d18f3d211fae08e91c
Sujal Agrawal <113822996+ZORO-JUROO@users.noreply.github.com>
2025-10-03 21:58:17 +0530

    Time: 23 ms (6.04%) | Memory: 44.9 MB (17.00%) - LeetSync

---
+++ DP/38-count-and-say/count-and-say.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String countAndSay(int n) {
+        StringBuilder arr[]=new StringBuilder[n];
+        arr[0] = new StringBuilder();
+        arr[0].append("1");
+        for(int i=1;i<n;i++)
+        {
+            arr[i]=new StringBuilder();
+            String s=arr[i-1].toString();
+            char c=s.charAt(0);
+                        System.out.println(s);
+            int j=1,m=s.length();
+            while(j<=m)
+            {
+                int k=1;
+                while(j<m && s.charAt(j)==c)
+                {
+                    j++;k++;
+                }
+                arr[i].append(Integer.toString(k)+c);
+                if(j<m){c=s.charAt(j);}j++;
+            }
+        }
+        return arr[n-1].toString();
+    }
+}
